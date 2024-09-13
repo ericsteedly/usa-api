fcfc811ee5753691269e6f2a9d51d8726f677085
Eric Steedly <ericsteedly@gmail.com>
2024-09-13 15:11:52 -0500

    updated settings.py to include restframework and authtoken, also fixed seeddata script

--- seed_data.sh
+++ seed_data.sh
@@ -5,6 +5,7 @@ rm -rf ./usaapi/migrations
 python manage.py makemigrations usaapi
 python manage.py migrate
 python manage.py loaddata users
+python manage.py loaddata tokens
 python manage.py loaddata states
 python manage.py loaddata cities
 python manage.py loaddata userdetail
